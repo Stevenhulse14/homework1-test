b902218cf1a5fd8fe15bd526fc4138b5c2a3b78d
steven <shotta3xd@gmail.com>
2026-02-03 15:21:05 -0500

    added LAB

---
+++ week2/intprinter.java
@@ -0,0 +1,12 @@
+public class intprinter <T> {
+    
+    private int number;
+
+    public intprinter(int number) {
+        this.number = number;
+    }
+
+    public void printNumber() {
+        System.out.println(number);
+    }
+}
---
+++ week2/start.java
@@ -0,0 +1,8 @@
+public class start {
+    
+   public static void main(String[] args) {
+    
+        intprinter printer = new intprinter(42);
+        printer.printNumber();
+    }
+}
